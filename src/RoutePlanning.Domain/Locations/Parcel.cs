193c3bad1ff89a427201afb9fdd38b548570293f
Marie Øverby <maov@netcompany.com>
2023-11-09 13:01:54 +0100

    Parcel class started on

---
+++ src/RoutePlanning.Domain/Locations/Parcel.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+using Netcompany.Net.DomainDrivenDesign.Models;
+
+namespace RoutePlanning.Domain.Locations;
+
+[DebuggerDisplay("{Value} km")]
+public sealed record Parcel : IValueObject
+{
+    public string[]? Type { get; set; }
+    public double Weight { get; set; }
+    public bool Signed { get; set; }
+}
