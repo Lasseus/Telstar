577b989d3a832d385bde01bbf965146eb7f1ce9c
adequateemmelie <emmeliechristensen@hotmail.com>
2023-11-09 13:52:15 +0100

    Added locations
    Locations added; still need connections

--- src/RoutePlanning.Client.Web/DatabaseInitialization.cs
+++ src/RoutePlanning.Client.Web/DatabaseInitialization.cs
@@ -40,6 +40,92 @@ public static class DatabaseInitialization
         await context.AddAsync(warsaw);
 
         // OUR LOCATIONS:
+        var tanger = new Location("TANGER");
+        await context.AddAsync(tanger);
+
+        var marrakesh = new Location("MARRAKESH");
+        await context.AddAsync(marrakesh);
+
+        var tunis = new Location("TUNIS");
+        await context.AddAsync(tunis);
+
+        var sahara = new Location("SAHARA");
+        await context.AddAsync(sahara);
+
+        var tripoli = new Location("TRIPOLI");
+        await context.AddAsync(tripoli);
+
+        var dakar = new Location("DAKAR");
+        await context.AddAsync(dakar);
+
+        var sierraleone = new Location("SIERRA LEONE");
+        await context.AddAsync(sierraleone);
+
+        var guldkysten = new Location("GULDKYSTEN");
+        await context.AddAsync(guldkysten);
+
+        var timbuktu = new Location("TIMBUKTU");
+        await context.AddAsync(timbuktu);
+
+        var slavekysten = new Location("SLAVE KYSTEN");
+        await context.AddAsync(slavekysten);
+
+        var congo = new Location("CONGO");
+        await context.AddAsync(congo);
+
+        var wadai = new Location("WADAI");
+        await context.AddAsync(wadai);
+
+        var darfur = new Location("DARFUR");
+        await context.AddAsync(darfur);
+
+        var daifur = new Location("DAIFUR");
+        await context.AddAsync(daifur);
+
+        var omdurman = new Location("OMDURMAN");
+        await context.AddAsync(omdurman);
+
+        var cairo = new Location("CAIRO");
+        await context.AddAsync(cairo);
+
+        var suakin = new Location("SUAKIN");
+        await context.AddAsync(suakin);
+
+        var bahrelghazal = new Location("BAHREL GHAZAL");
+        await context.AddAsync(bahrelghazal);
+
+        var victoriasoen = new Location("VICTORIASØEN");
+        await context.AddAsync(victoriasoen);
+
+        var addisabeba = new Location("ADDIS ABEBA");
+        await context.AddAsync(addisabeba);
+
+        var kapguardafui = new Location("KAP GUARDAFUI");
+        await context.AddAsync(kapguardafui);
+
+        var zanzibar = new Location("ZANZIBAR");
+        await context.AddAsync(zanzibar);
+
+        var mocambique = new Location("MOCAMBIQUE");
+        await context.AddAsync(mocambique);
+
+        var kabalo = new Location("KABALO");
+        await context.AddAsync(kabalo);
+
+        var luanda = new Location("LUANDA");
+        await context.AddAsync(luanda);
+
+        var dragebjerget = new Location("DRAGEBJERGET");
+        await context.AddAsync(dragebjerget);
+
+        var victoriafaldene = new Location("VICTORIAFALDENE");
+        await context.AddAsync(victoriafaldene);
+
+        var hvalbugten = new Location("HVALBUGTEN");
+        await context.AddAsync(hvalbugten);
+
+        var kapstaden = new Location("KAPSTADEN");
+        await context.AddAsync(kapstaden);
 
         // OLD CONNECTIONS:
         CreateTwoWayConnection(berlin, warsaw, 573);
